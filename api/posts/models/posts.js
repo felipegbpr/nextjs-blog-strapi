02162588003de35952984f067089cdc4715405e3
Felipe Borges <felipetecacc@gmail.com>
2024-11-28 20:07:32 -0300

    Added lifecycle-hooks afterCreate and afterUpdate

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -8,10 +8,12 @@ const axios = require('axios');
  */
 
 module.exports = {
-  async afterCreate(result, data) {
-    axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048')
-  },
-  async afterUpdate(result, params, data) {
-    axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048')
-  },
+  lifecycles: {
+    async afterCreate(result, data) {
+      axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048')
+    },
+    async afterUpdate(result, params, data) {
+      axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048')
+    },
+  }
 };
