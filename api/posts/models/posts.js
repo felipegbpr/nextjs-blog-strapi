ce4949ec969928c9cbdde35ce6b5925c2b57e483
Felipe Borges <felipetecacc@gmail.com>
2024-11-29 16:21:02 -0300

    Removed netlify webhook from lifecycles

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -8,13 +8,13 @@ const axios = require('axios');
  */
 
 module.exports = {
-  lifecycles: {
-    async afterCreate(result, data) {
-      axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048');
-    },
+  // lifecycles: {
+  //   async afterCreate(result, data) {
+  //     axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048');
+  //   },
 
-    async afterUpdate(result, params, data) {
-      axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048');
-    },
-  }
+  //   async afterUpdate(result, params, data) {
+  //     axios.post('https://api.netlify.com/build_hooks/6748aec93a5a8e4347c61048');
+  //   },
+  // }
 };
